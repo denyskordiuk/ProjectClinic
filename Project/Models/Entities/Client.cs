4f8a286b00df37f6971452dae8ed03c9aad3dbcf
Denys Kordiuk <denys.kordiuk.lviv@gmail.com>
2021-07-14 16:18:15 +0300

    Added regular expression

--- Project/Models/Entities/Client.cs
+++ Project/Models/Entities/Client.cs
@@ -8,10 +8,16 @@ namespace Project.Models.Entities
 {
     public class Client
     {
+        [Display(Name = "Full Name")]
+        [RegularExpression(@"^([À-ßA-Z]|[À-ßA-Z][\x27à-ÿa-z]{1,}|[À-ßA-Z][\x27à-ÿa-z]{1,}\-([À-ßA-Z][\x27à-ÿa-z]{1,}|(îãëû)|(êûçû)))\040[À-ßA-Z][\x27à-ÿa-z]{1,}(\040[À-ßA-Z][\x27à-ÿa-z]{1,})?$", ErrorMessage = "Incorrect name example(Ivanov Ivan Ivanovich)")]
         public string FullName { get; set; }
+        [RegularExpression(@"^(?:1(?:00?|\d)|[2-5]\d|[6-9]\d?)$", ErrorMessage = "Incorrect age")]
         public int Age { get; set; }
         public string Adress { get; set; }
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)\@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])", ErrorMessage = "Incorrect mail example(example@gmail.com)")]
         public string Mail { get; set; }
+        [Key]
+        [RegularExpression(@"^\+?3?8?(0\d{9})$", ErrorMessage = "Incorrect phone number")]
         public string PhoneNumber { get; set; }
         public PayMethod PayMethod { get; set; }
     }
--- Project/Models/Entities/Doctor.cs
+++ Project/Models/Entities/Doctor.cs
@@ -8,11 +8,14 @@ namespace Project.Models.Entities
 {
     public class Doctor
     {
+        [Key]
         public string License { get; set; }
         [Display(Name = "Doctor Name")]
+        [RegularExpression(@"^([А-ЯA-Z]|[А-ЯA-Z][\x27а-яa-z]{1,}|[А-ЯA-Z][\x27а-яa-z]{1,}\-([А-ЯA-Z][\x27а-яa-z]{1,}|(оглы)|(кызы)))\040[А-ЯA-Z][\x27а-яa-z]{1,}(\040[А-ЯA-Z][\x27а-яa-z]{1,})?$", ErrorMessage = "Incorrect name example(Ivanov Ivan Ivanovich)")]
         public string DoctorName { get; set; }
         public int Experience { get; set; }
         [Display(Name = "Phone Number")]
+        [RegularExpression(@"^\+?3?8?(0\d{9})$", ErrorMessage = "Incorrect phone number")]
         public string PhoneNumber { get; set; }
         public int Cabinet { get; set; }
         [Display(Name = "Doctor Status")]
